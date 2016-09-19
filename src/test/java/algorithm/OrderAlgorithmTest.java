407ba332e2d64e151764ea57ee3943533dda6882
Andre Almeida <aalmeida@thoughtworks.com>
2016-09-19 14:35:49 -0300

    add OrderAlgorithmTest

---
+++ src/test/java/algorithm/OrderAlgorithmTest.java
@@ -0,0 +1,56 @@
+package algorithm;
+
+import model.InventoryItem;
+import model.OrderItem;
+import model.ShippingMethod;
+import model.dto.Request;
+import model.dto.Response;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+import repository.Repository;
+import strategy.NoneStrategy;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+
+public class OrderAlgorithmTest {
+    private OrderAlgorithm orderAlgorithm = new OrderAlgorithm();
+
+    @Test
+    public void shouldReturnBrazilCountryWhenMouseProductIsPassed() throws Exception {
+        Request request = new Request(asList(
+                new InventoryItem("Brazil", "Keyboard", 2),
+                new InventoryItem("France", "Mouse", 2)),
+                new Repository().getWarehouseRepository(),
+                ShippingMethod.DHL,
+                asList( new OrderItem("Keyboard",2)),
+                new NoneStrategy());
+
+        Response expected = new Response(asList(new InventoryItem("Brazil", "Keyboard" , 2)));
+
+        Response actual = orderAlgorithm.execute(request);
+        assertThat(actual, Is.is(expected));
+    }
+
+    @Test
+    public void shouldReturnBrazilAndArgentinaCountriesWhenMouseAndMonitorProductsIsPassed() throws Exception {
+
+        Request request = new Request(asList(
+                          new InventoryItem("Brazil", "Mouse", 2),
+                          new InventoryItem("Chile", "Keyboard", 3),
+                          new InventoryItem("Argentina", "Monitor", 6)),
+                new Repository().getWarehouseRepository(),
+                ShippingMethod.DHL,
+                asList( new OrderItem("Mouse", 2),
+                        new OrderItem("Monitor", 6)),
+                new NoneStrategy());
+
+        Response actual = orderAlgorithm.execute(request);
+        Response expected = new Response(asList(new InventoryItem("Brazil", "Mouse", 2), new InventoryItem("Argentina", "Monitor", 6)));
+
+        assertThat(actual, is(expected));
+    }
+
+}
